6c9781de4e33ef5287836893b20906892091c7ca
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2023-06-04 23:56:20 +0300

    Initial Commit

---
+++ daf-yomi/daf_yomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  daf_yomiApp.swift
+//  daf-yomi
+//
+//  Created by Yehuda Neufeld on 04/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct daf_yomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
